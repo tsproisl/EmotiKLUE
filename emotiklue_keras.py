84534b32c5b7f88faa34e69bf0613a4c87cdb39f
Thomas Proisl <thomas.proisl@fau.de>
2018-06-21 17:13:44 +0200

    Make character-level LSTMs optional

--- emotiklue_keras.py
+++ emotiklue_keras.py
@@ -22,6 +22,7 @@ def arguments():
     parser.add_argument("--train", type=os.path.abspath, required=True, help="Dataset for training")
     parser.add_argument("--val", type=os.path.abspath, required=True, help="Dataset for validation")
     parser.add_argument("--embeddings", type=os.path.abspath, required=True, help="Word embeddings")
+    parser.add_argument("--chars", action="store_true", help="Use additional character-level LSTMs")
     parser.add_argument("--gpu", type=int, default=0, help="Number of GPUs to use")
     return parser.parse_args()
 
@@ -70,19 +71,21 @@ def vectorize_words(sequence, mapping, reverse=False):
 
 
 def main():
+    args = arguments()
+
     WORD_EMBEDDING_DIM = 100
     CHAR_EMBEDDING_DIM = 16
     WORD_LSTM_DIM = WORD_EMBEDDING_DIM
     CHAR_LSTM_DIM = CHAR_EMBEDDING_DIM
-    # TEST
-    DENSE_DIM = WORD_LSTM_DIM + CHAR_LSTM_DIM
-    # DENSE_DIM = WORD_LSTM_DIM
+    if args.chars:
+        DENSE_DIM = WORD_LSTM_DIM + CHAR_LSTM_DIM
+    else:
+        DENSE_DIM = WORD_LSTM_DIM
     DROPOUT = 0.2
     RECURRENT_DROPOUT = 0.0
     BATCH_SIZE = 160
     EPOCHS = 10
 
-    args = arguments()
     train_lw, train_rw, train_lc, train_rc, train_tgt, vocabulary, classes = read_dataset(args.train)
     val_lw, val_rw, val_lc, val_rc, val_tgt, _, _ = read_dataset(args.val)
     embeddings_index = read_glove(args.embeddings)
@@ -148,9 +151,10 @@ def main():
     lstm_rc = keras.layers.LSTM(CHAR_LSTM_DIM, dropout=DROPOUT, recurrent_dropout=RECURRENT_DROPOUT)(embedding_rc)
 
     # concatenate
-    # TEST
-    lstm_out = keras.layers.Concatenate(axis=1)([lstm_lw, lstm_rw, lstm_lc, lstm_rc])
-    # lstm_out = keras.layers.Concatenate(axis=1)([lstm_lw, lstm_rw])
+    if args.chars:
+        lstm_out = keras.layers.Concatenate(axis=1)([lstm_lw, lstm_rw, lstm_lc, lstm_rc])
+    else:
+        lstm_out = keras.layers.Concatenate(axis=1)([lstm_lw, lstm_rw])
 
     # dense layers
     dense01 = keras.layers.Dense(DENSE_DIM, activation="tanh")(lstm_out)
@@ -159,9 +163,10 @@ def main():
     predictions = keras.layers.Dense(len(classes), activation="softmax")(dropout01)
 
     with tf.device('/cpu:0'):
-        # TEST
-        model = keras.models.Model(inputs=[input_lw, input_rw, input_lc, input_rc], outputs=predictions)
-        # model = keras.models.Model(inputs=[input_lw, input_rw], outputs=predictions)
+        if args.chars:
+            model = keras.models.Model(inputs=[input_lw, input_rw, input_lc, input_rc], outputs=predictions)
+        else:
+            model = keras.models.Model(inputs=[input_lw, input_rw], outputs=predictions)
     if args.gpu > 1:
         parallel_model = keras.utils.multi_gpu_model(model, gpus=args.gpu)
         parallel_model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
@@ -169,11 +174,12 @@ def main():
                            validation_data=([val_left_words, val_right_words, val_left_chars, val_right_chars], val_targets))
     else:
         model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-        # TEST
-        model.fit([train_left_words, train_right_words, train_left_chars, train_right_chars], targets, batch_size=BATCH_SIZE, epochs=EPOCHS,
-                  validation_data=([val_left_words, val_right_words, val_left_chars, val_right_chars], val_targets))
-        # model.fit([train_left_words, train_right_words], targets, batch_size=BATCH_SIZE, epochs=EPOCHS,
-        #           validation_data=([val_left_words, val_right_words], val_targets))
+        if args.chars:
+            model.fit([train_left_words, train_right_words, train_left_chars, train_right_chars], targets, batch_size=BATCH_SIZE, epochs=EPOCHS,
+                      validation_data=([val_left_words, val_right_words, val_left_chars, val_right_chars], val_targets))
+        else:
+            model.fit([train_left_words, train_right_words], targets, batch_size=BATCH_SIZE, epochs=EPOCHS,
+                      validation_data=([val_left_words, val_right_words], val_targets))
     model.save("%s.h5" % args.model)
     with open("%s.maps" % args.model, mode="w") as f:
         json.dump((word_to_idx, tgt_to_idx), f)
