5dfcada545cbefca39cda944f0c3b7045f1f8dc1
Thomas Proisl <thomas.proisl@fau.de>
2018-06-29 09:31:05 +0200

    Use combination of tweets and ENCOW sentences

--- train_models.sh
+++ train_models.sh
@@ -7,11 +7,11 @@ time ./emotiklue_keras.py train -m model_train_w2v_cbow --val /ccl/projects/IEST
 time ./emotiklue_keras.py train -m model_train_glove --val /ccl/projects/IEST/dev-v3.csv --embeddings /cip/corpora/DSM/GloVe/glove.twitter.27B.100d.txt.gz /ccl/projects/IEST/train-v3.csv_tokenized.txt
 ./emotiklue_keras.py test -m model_train_glove /ccl/projects/IEST/dev-v3.csv
 
-time ./emotiklue_keras.py train -m model_extra_w2v_skipgram --val /ccl/projects/IEST/dev-v3.csv --embeddings /ccl/projects/IEST/tweets_w2v_skipgram_100.txt.gz /ccl/projects/IEST/additional_training_data_balanced.csv
+time ./emotiklue_keras.py train -m model_extra_w2v_skipgram --val /ccl/projects/IEST/dev-v3.csv --embeddings /ccl/projects/IEST/tweets_w2v_skipgram_100.txt.gz /ccl/projects/IEST/additional_training_data_tweets+encow.csv
 ./emotiklue_keras.py test -m model_extra_w2v_skipgram /ccl/projects/IEST/dev-v3.csv
-time ./emotiklue_keras.py train -m model_extra_w2v_cbow --val /ccl/projects/IEST/dev-v3.csv --embeddings /ccl/projects/IEST/tweets_w2v_cbow_100.txt.gz /ccl/projects/IEST/additional_training_data_balanced.csv
+time ./emotiklue_keras.py train -m model_extra_w2v_cbow --val /ccl/projects/IEST/dev-v3.csv --embeddings /ccl/projects/IEST/tweets_w2v_cbow_100.txt.gz /ccl/projects/IEST/additional_training_data_tweets+encow.csv
 ./emotiklue_keras.py test -m model_extra_w2v_cbow /ccl/projects/IEST/dev-v3.csv
-time ./emotiklue_keras.py train -m model_extra_glove --val /ccl/projects/IEST/dev-v3.csv --embeddings /cip/corpora/DSM/GloVe/glove.twitter.27B.100d.txt.gz /ccl/projects/IEST/additional_training_data_balanced.csv
+time ./emotiklue_keras.py train -m model_extra_glove --val /ccl/projects/IEST/dev-v3.csv --embeddings /cip/corpora/DSM/GloVe/glove.twitter.27B.100d.txt.gz /ccl/projects/IEST/additional_training_data_tweets+encow.csv
 ./emotiklue_keras.py test -m model_extra_glove /ccl/projects/IEST/dev-v3.csv
 
 time ./emotiklue_keras.py retrain -m model_extra_w2v_skipgram --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
