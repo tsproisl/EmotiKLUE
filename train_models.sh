1b6297860ef0d933c37f9305b7738505c1c13326
Thomas Proisl <thomas.proisl@fau.de>
2018-06-30 08:59:38 +0200

    evaluate retrain and adapt

--- train_models.sh
+++ train_models.sh
@@ -14,9 +14,16 @@ time ./emotiklue_keras.py train -m model_extra_w2v_cbow --val /ccl/projects/IEST
 time ./emotiklue_keras.py train -m model_extra_glove --val /ccl/projects/IEST/dev-v3.csv --embeddings /cip/corpora/DSM/GloVe/glove.twitter.27B.100d.txt.gz /ccl/projects/IEST/additional_training_data_tweets+encow.csv
 ./emotiklue_keras.py test -m model_extra_glove /ccl/projects/IEST/dev-v3.csv
 
+time ./emotiklue_keras.py adapt -m model_extra_w2v_skipgram --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
+./emotiklue_keras.py test -m model_extra_w2v_skipgram_adapt /ccl/projects/IEST/dev-v3.csv
+time ./emotiklue_keras.py adapt -m model_extra_w2v_cbow --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
+./emotiklue_keras.py test -m model_extra_w2v_cbow_adapt /ccl/projects/IEST/dev-v3.csv
+time ./emotiklue_keras.py adapt -m model_extra_glove --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
+./emotiklue_keras.py test -m model_extra_glove_adapt /ccl/projects/IEST/dev-v3.csv
+
 time ./emotiklue_keras.py retrain -m model_extra_w2v_skipgram --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
-./emotiklue_keras.py test -m model_extra_w2v_skipgram /ccl/projects/IEST/dev-v3.csv
+./emotiklue_keras.py test -m model_extra_w2v_skipgram_retrain /ccl/projects/IEST/dev-v3.csv
 time ./emotiklue_keras.py retrain -m model_extra_w2v_cbow --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
-./emotiklue_keras.py test -m model_extra_w2v_cbow /ccl/projects/IEST/dev-v3.csv
+./emotiklue_keras.py test -m model_extra_w2v_cbow_retrain /ccl/projects/IEST/dev-v3.csv
 time ./emotiklue_keras.py retrain -m model_extra_glove --val /ccl/projects/IEST/dev-v3.csv /ccl/projects/IEST/train-v3.csv_tokenized.txt
-./emotiklue_keras.py test -m model_extra_glove /ccl/projects/IEST/dev-v3.csv
+./emotiklue_keras.py test -m model_extra_glove_retrain /ccl/projects/IEST/dev-v3.csv
